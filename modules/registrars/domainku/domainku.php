d2e23d3d74006500b781f481aad88ef8a37a648d
root <rangga.praduwiratna@gmail.com>
2015-07-15 21:37:35 +0700

    change folder structures & add hook for formatting phone number

--- modules/registrars/domainku/domainku.php
+++ modules/registrars/domainku/domainku.php
@@ -1,10 +1,25 @@
 <?php
 /**
+ * Copyright (c) 2015, Infinys System Indonesia
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
  * This is registar command module for connecting to DomainCloud API.
  *
  * @package    DCRegistrarModules
  * @author     Infinys System Indonesia
- * @copyright  Copyright (c) Infinys System Indonesia. 2014
+ * @copyright  Copyright (c) Infinys System Indonesia. 2015
  * @license    http://www.isi.co.id/
  * @version    $Id$
  * @link       http://www.isi.co.id/
